7b25fd1eb8730cd252861201707d5a87e402f5a2
the-professor1 <144011719+the-professor1@users.noreply.github.com>
2024-05-26 01:53:18 +0530

    nmmnmn modifiesd

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const headingEle = React.createElement("h1",{id:"heading", class:"myClass"}, "hellow worlsdfghjgfdgd from the react!");
 const childEle = React.createElement("div", {id:"child", class:"child-ele"}, headingEle);
-const parentEle = React.createElement("div", {id:"parent", class:"parent-ele"}, childEle);
+const parentEle = React.createElement("div", {id:"parent", class:"parent-ele"}, [headingEle, childEle, headingEle], [headingEle, childEle, headingEle]);
 console.log(parentEle);
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(parentEle);
